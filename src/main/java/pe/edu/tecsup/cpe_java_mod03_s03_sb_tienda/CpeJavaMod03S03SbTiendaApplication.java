d624cae2b1ee22269daa1d14355c9b042432f85a
jgomezz <jgomezz@gmail.com>
2026-01-25 10:37:04 -0500

    first changes

---
+++ src/main/java/pe/edu/tecsup/cpe_java_mod03_s03_sb_tienda/CpeJavaMod03S03SbTiendaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.cpe_java_mod03_s03_sb_tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpeJavaMod03S03SbTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpeJavaMod03S03SbTiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/tecsup/cpe_java_mod03_s03_sb_tienda/CpeJavaMod03S03SbTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.cpe_java_mod03_s03_sb_tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpeJavaMod03S03SbTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
