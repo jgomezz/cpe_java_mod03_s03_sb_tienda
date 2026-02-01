c0bc38fe36ea5db8a4ce3c80e0818ad7fbef9a8b
jgomezz <jgomezz@gmail.com>
2026-02-01 11:45:46 -0500

    creación de una página de login personalizada

--- src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
+++ src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,4 +57,53 @@ public class SpringSecurityConfig {
     }
 
     //*/
+
+    // Autorizaciones
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // Configuración de accesos
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/", "webjars/**", "/css/**", "/js/**", "/images/**", "/error/**").permitAll()
+                        .requestMatchers("/productos/**").authenticated()
+                )
+                // Configuración de formulario de login
+                .formLogin((form) -> form
+                        .loginPage("/login").permitAll()
+                        .loginProcessingUrl("/authenticate")
+                        .defaultSuccessUrl("/", true)
+                )
+                // Configuración de logout
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                )
+                // Change csrf
+                .csrf( (csrf) -> csrf.disable()
+                );
+
+        return http.build();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
