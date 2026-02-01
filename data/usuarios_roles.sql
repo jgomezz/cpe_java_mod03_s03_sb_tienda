87ee9568f167c99979729b45791c333639d0c79d
jgomezz <jgomezz@gmail.com>
2026-02-01 08:31:01 -0500

    datos de usuarios y roles

---
+++ data/usuarios_roles.sql
@@ -0,0 +1,10 @@
+INSERT INTO `roles` VALUES (1,'Administrador'),(2,'Ventas'),(3,'Almacen');
+
+
+INSERT INTO `usuarios` VALUES
+(1,1,'ebenites@tecsup.edu.pe','tecsup','Erick','Benites','M','1990-05-08','954658512','Av. Peru 123','1'),
+(2,2,'jaraujo@tecsup.edu.pe','tecsup','Janeth','Araujo','F',NULL,NULL,NULL,'1'),
+(3,3,'jflores@tecsup.edu.pe','tecsup','Jorge','Flores','M',NULL,NULL,NULL,'1'),
+(4,2,'jfarfan@tecsup.edu.pe','tecsup','Jaime','Farfan','M','2019-10-09','3258545','Av. Cascanueces 2221 Santa Anita','1'),
+(5,1,'erick.benites@gmail.com','metallica','Erick','Benites','M','2019-12-02','3258545','Av. Cascanueces 2221 Santa Anita','1'),
+(6,2,'lmaza@tecsup.edu.pe','tecsup','Luis','Maza','M','2019-12-10','3258545','Av. Cascanueces 2221 Santa Anita','1');
